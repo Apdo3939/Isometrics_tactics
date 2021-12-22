33018c9250e959436393258cec718ebb2d937015
Apdo3939 <apdo3939@hotmail.com>
2021-12-22 17:22:28 -0300

    Add animations 01

---
+++ Assets/Scripts/Amimation/SpriteLoader.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLoader : MonoBehaviour
+{
+    [Header("Adress path")]
+    public string path;
+    public List<Animation2D> animation2Ds;
+}
